2afd126ffacffd72fe2d326030e7eed13296e924
juan <21977733+juanmed@users.noreply.github.com>
2019-03-19 20:37:38 +0900

    Tuned PID gains for better control accuracy. Modifiede estimator launch file and now moved rqt_plot to control_trajectory launch file.

--- tests/scripts/uav_reftraj_publisher.py
+++ tests/scripts/uav_reftraj_publisher.py
@@ -94,7 +94,7 @@ class Trajectory_Generator():
         # compute flat output trajectory
         self.sol_x = qp_solution.qp_solution(self.order, self.n, self.waypoint, self.t, self.keyframe, self.current_state)
         # draw trajectory in plot
-        draw_trajectory.draw_trajectory(self.sol_x, self.order, self.waypoint, self.n, self.t, self.keyframe)
+        #draw_trajectory.draw_trajectory(self.sol_x, self.order, self.waypoint, self.n, self.t, self.keyframe)
 
         # for counting gate
         self.passed_gate = 0
