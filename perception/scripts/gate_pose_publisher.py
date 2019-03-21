2d85e738f129638faca3b3305c3db0e4547e1d44
juan <21977733+juanmed@users.noreply.github.com>
2019-03-21 23:00:21 +0900

    Fixes: - Inverted published transformation in perception package: previous sent Gate->Drone frame transform. Now is Drone->Gate transform.
    Features:
    - Added GatePose and GatePoseArray msg type to publish gate poses
    that have at least 4 markers visible in the camera plane.
    - Publish the messages in a new topic

--- perception/scripts/gate_pose_publisher.py
+++ perception/scripts/gate_pose_publisher.py
@@ -10,6 +10,7 @@ from utils import *
 
 from flightgoggles.msg import IRMarker, IRMarkerArray
 from std_msgs.msg import String
+from perception.msg import GatePoseArray, GatePose
 
 
 class Gate_Pose_Estimator():
@@ -20,13 +21,14 @@ class Gate_Pose_Estimator():
         self.init_pos, self.init_ori = self.get_init_pose()
 
         # publisher
-        self.gate_publisher = rospy.Publisher("/riseq/perception/gate_pose", IRMarkerArray, queue_size = 10)
+        self.gate_publisher = rospy.Publisher("/riseq/perception/gate_poses", GatePoseArray, queue_size = 10)
 
         # subscribe to IR Marker topic
         rospy.Subscriber("/uav/camera/left/ir_beacons", IRMarkerArray, self.read_estimate_publish)
 
         self.gates = {}
 
+        # This camera intrinsic is obtained from topic uav/camera/left/camera_info
         self.camera_intrinsic = np.zeros((3,3), dtype = 'float64')
         self.camera_intrinsic[0] = np.array([548.4088134765625, 0.0, 512.0])
         self.camera_intrinsic[1] = np.array([0.0, 548.4088134765625, 384.0])
@@ -41,10 +43,9 @@ class Gate_Pose_Estimator():
         # and the value is a list of its corresponding ir_markers 
 
         self.gates = {}
+        gate_poses = []
 
-        marker_count = len(ir_markers.markers)
         # extract and organize all markers in a dictionary
-        t1 = rospy.Time.now().to_sec()
         for marker in ir_markers.markers:
             gate_name = marker.landmarkID.data
             # if this gate is not in dictionary
@@ -56,30 +57,43 @@ class Gate_Pose_Estimator():
                 self.gates[gate_name] = []
                 self.gates[gate_name].append( (int(marker.markerID.data), np.array([marker.x, marker.y])))
         
+
         # compute Rotation, translation for gates having >=3 markers visible
         for gate_name in self.gates:
             # if 3 markers are present, is enough for P3P 
             markers = self.gates[gate_name]
             if( len(markers) > 3):
-                print("**{} has {} visible points".format(gate_name, len(markers)) )
-                # get Rotation matrix and translation
-                R, t = self.estimate_gate_pose(gate_name, markers)
+                #print("**{} has {} visible points".format(gate_name, len(markers)) )
+                # get quaternion and translation: drone frame to gate frame
+                q_dg, t_dg = self.estimate_gate_pose(gate_name, markers)
+
+                gate_pose_msg = GatePose()
+                gate_pose_msg.gate_name = gate_name
+
+                gate_pose_msg.pose.position.x = t_dg[0][0]
+                gate_pose_msg.pose.position.y = t_dg[1][0]
+                gate_pose_msg.pose.position.z = t_dg[2][0]
+
+                gate_pose_msg.pose.orientation.x = q_dg[0]
+                gate_pose_msg.pose.orientation.y = q_dg[1]
+                gate_pose_msg.pose.orientation.z = q_dg[2]
+                gate_pose_msg.pose.orientation.w = q_dg[3]
+
+                gate_poses.append(gate_pose_msg)
 
             else:
                 #print("{} has {} visible points".format(gate_name, len(markers)))
                 continue
 
+        # Prepare and send list of gate poses 
+        gate_poses_msg = GatePoseArray()
+        gate_poses_msg.header.stamp = rospy.Time.now()
+        gate_poses_msg.header.frame_id = "uav/gates"
+        gate_poses_msg.gate_poses = gate_poses
 
-        t2 = rospy.Time.now().to_sec()
+        self.gate_publisher.publish(gate_poses_msg)
+        rospy.loginfo(gate_poses_msg)
 
-        #print("t1: {}".format(t1))
-        #print("t2: {}".format(t2))
-        #print("Duration: {:.10f}".format( t2-t1 ))
-
-
-
-        #self.gate_publisher.publish(ir_markers)
-        #rospy.loginfo("Received markers.")
 
     def estimate_gate_pose(self, gate_name, markers):
 
@@ -127,11 +141,37 @@ class Gate_Pose_Estimator():
 
         #print("Points 2D: {}".format(points_2D))
 
-        R, t = pnp(gate_markers_3d_local, points_2D, self.camera_intrinsic)
-        print("Rotation : {}".format(R))
-        print("Translation: {}".format(t))
+        # Calculate gate pose using pnp algorithm
+        # The returned rotation matrix R, and translation t are from the Gate frame
+        # to the Drone, and Z,Y axis are interchanged
+        R_gd, t_gd = pnp(gate_markers_3d_local, points_2D, self.camera_intrinsic)
 
-        return R,t
+        # translation from drone to gate
+        t_dg = np.zeros_like(t_gd)
+        t_dg[0][0] = t_gd[0][0]
+        t_dg[1][0] = t_gd[2][0]
+        t_dg[2][0] = t_gd[1][0]
+        t_dg = -t_dg
+
+        # Rotation from drone to gate
+        R = R_gd.T
+        R_dg = np.zeros_like(R)
+        R_dg[:,0] = R[:,0]
+        R_dg[:,1] = R[:,2]
+        R_dg[:,2] = R[:,1]
+
+        dummy1 = np.zeros((3,1))
+        dummy2 = np.zeros((1,4))
+        dummy2[0][3] = 1.0
+        R_dg = np.concatenate((R_dg,dummy1), axis = 1)
+        R_dg = np.concatenate((R_dg, dummy2), axis = 0)
+        
+        q_dg = tf.transformations.quaternion_from_matrix(R_dg)
+
+        #print("Rotation Drone -> Gate : {}".format(q_dg))
+        #print("Translation Drone -> Gate : {}".format(t_dg))
+
+        return q_dg, t_dg
 
 
 
@@ -163,7 +203,7 @@ if __name__ == '__main__':
         try:
             wait_time = int(rospy.get_param("riseq/perception_wait"))
         except:
-            wait_time = 40
+            wait_time = 2
 
         while( rospy.Time.now().to_sec() < wait_time ):
             if( ( int(rospy.Time.now().to_sec()) % 1) == 0 ):
